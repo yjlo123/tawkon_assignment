929f5a39cdcc6c2fcda3cc956e9291de1c43e44b
Siwei <liusiwei.yjlo@gmail.com>
2013-12-15 17:38:55 +0800

    BaseAdapter / ViewHolder

--- src/com/yjlo/test/DetailPersonActivity.java
+++ src/com/yjlo/test/DetailPersonActivity.java
@@ -12,54 +12,49 @@ import android.widget.ImageView;
 import android.widget.TextView;
 public class DetailPersonActivity extends Activity {
 	
-    static TextView first_name;
-    static TextView last_name;
-    static TextView description;
-    static TextView country;
-    static String person_name;
-    static ImageView avatar;
-    Button share;
+    private TextView firstName;
+    private TextView lastName;
+    private TextView description;
+    private TextView country;
+    private ImageView avatar;
+    private Button share;
     
-    static String got_id;
-    static String got_first_name;
-    static String got_last_name;
-    static String got_description;
-    static String got_country;
-    static String got_pic_url;
+    private String gotFirstName;
+    private String gotLastName;
+    private String gotDescription;
+    private String gotCountry;
+    private String gotPicUrl;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.details);
 		
 		initialize();
 		Bundle got_bdl = getIntent().getExtras();
-		got_id = got_bdl.getString("id");
-		got_first_name = got_bdl.getString("first_name");
-		got_last_name = got_bdl.getString("last_name");
-		got_description = got_bdl.getString("description");
-		got_country = got_bdl.getString("country");
-		got_pic_url = got_bdl.getString("pic_url");
+		gotFirstName = got_bdl.getString("first_name");
+		gotLastName = got_bdl.getString("last_name");
+		gotDescription = got_bdl.getString("description");
+		gotCountry = got_bdl.getString("country");
+		gotPicUrl = got_bdl.getString("pic_url");
 		
-		first_name.setText( got_first_name);
-		last_name.setText(got_last_name);
-		description.setText(got_description);
-		country.setText(got_country);
+		firstName.setText( gotFirstName);
+		lastName.setText(gotLastName);
+		description.setText(gotDescription);
+		country.setText(gotCountry);
 		
 		
         ImageLoader imageLoader = ImageLoader.getInstance();
         imageLoader.init(ImageLoaderConfiguration.createDefault(getBaseContext()));
-        imageLoader.displayImage(got_pic_url, avatar);
+        imageLoader.displayImage(gotPicUrl, avatar);
         
         share.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				Intent sendIntent = new Intent();
 				sendIntent.setAction(Intent.ACTION_SEND);
-				sendIntent.putExtra(Intent.EXTRA_TEXT, got_first_name+" "+got_last_name+": "+got_description);
+				sendIntent.putExtra(Intent.EXTRA_TEXT, gotFirstName+" "+gotLastName+": "+gotDescription);
 				sendIntent.setType("text/plain");
 				startActivity(sendIntent);
 			}
@@ -67,8 +62,8 @@ public class DetailPersonActivity extends Activity {
 	}
 	
 	private void initialize(){
-		first_name = (TextView)findViewById(R.id.tv_first_name);
-		last_name = (TextView)findViewById(R.id.tv_last_name);
+		firstName = (TextView)findViewById(R.id.tv_first_name);
+		lastName = (TextView)findViewById(R.id.tv_last_name);
 		description = (TextView)findViewById(R.id.tv_description);
 		country = (TextView)findViewById(R.id.tv_country);
 		
--- src/com/yjlo/test/MainActivity.java
+++ src/com/yjlo/test/MainActivity.java
@@ -17,21 +17,24 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
- 
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-//import android.widget.SimpleAdapter;
 import android.widget.TextView;
 //import android.widget.Toast;
- 
+
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.content.AsyncTaskLoader;
@@ -42,207 +45,280 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
 import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
-import com.yjlo.test.ExtendedSimpleAdapter;
- 
-public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks<JSONArray>{
-    ListView list;
-    TextView full_name;
 
-    List<HashMap<String, Object>> oslist = new ArrayList<HashMap<String, Object>>();
-    
-    //ImageLoader imageLoader=new ImageLoader(getBaseContext());
- 
-    //URL to get JSON Array
-    private static String url = "http://www.json-generator.com/j/bXtJLtTlQi?indent=4";
- 
-    //JSON Node Names 
-    private static final String FN = "first_name";
-    private static final String LN = "last_name";
-    private static final String ID = "id";
- 
-    JSONArray people_list = null;
-    private ProgressDialog pDialog;
- 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
- 
-        setContentView(R.layout.activity_main);
-        oslist = new ArrayList<HashMap<String, Object>>();
-        
-        initialize();
-        getSupportLoaderManager().initLoader(1, null, this).forceLoad();
-    }
-    
-	private void initialize(){
-        full_name = (TextView)findViewById(R.id.names);
+public class MainActivity extends FragmentActivity implements
+		LoaderManager.LoaderCallbacks<JSONArray> {
+	private ListView list;
 
+	List<HashMap<String, Object>> oslist = new ArrayList<HashMap<String, Object>>();
+
+	// ImageLoader imageLoader=new ImageLoader(getBaseContext());
+
+	// URL to get JSON Array
+	public final static String URL = "http://www.json-generator.com/j/bXtJLtTlQi?indent=4";
+
+	// JSON Node Names
+	private static final String FIRSTNAME = "first_name";
+	private static final String LASTNAME = "last_name";
+	private static final String DESCRIPTION = "description";
+	private static final String COUNTRY = "country";
+	private static final String PICTURE = "picture";
+	private static final String ID = "id";
+
+	private JSONArray peopleList = null;
+	private ProgressDialog pDialog;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+		oslist = new ArrayList<HashMap<String, Object>>();
+
+		list = (ListView) findViewById(R.id.list);
+		getSupportLoaderManager().initLoader(1, null, this).forceLoad();
 	}
-    
-  	@Override
+
+	// when the activity is destroyed, dismiss the dialog
+	protected void onDestroy() {
+		if (pDialog != null && pDialog.isShowing()) {
+			pDialog.dismiss();
+			pDialog = null;
+		}
+		super.onDestroy();
+	}
+
+	@Override
 	public Loader<JSONArray> onCreateLoader(int id, Bundle args) {
-        pDialog = new ProgressDialog(MainActivity.this);
-        pDialog.setMessage("Loading ...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(true);
-        pDialog.show();
-  		Loader<JSONArray> loader = new JSONParse(this);
+		// display loading dialog
+		pDialog = new ProgressDialog(MainActivity.this);
+		pDialog.setMessage("Loading ...");
+		pDialog.setIndeterminate(false);
+		pDialog.setCancelable(true);
+		pDialog.show();
+		Loader<JSONArray> loader = new JSONParse(this);
 		return loader;
 	}
-  	
+
 	public void onLoadFinished(Loader<JSONArray> loader, JSONArray json) {
-        pDialog.dismiss();
-        try {
-       	people_list = json;
-           for(int i = 0; i < people_list.length(); i++){
-               JSONObject person = people_list.getJSONObject(i);
+		// load finished, dismiss loading dialog
+		pDialog.dismiss();
 
-               String first_name = person.getString(FN);
-               String last_name = person.getString(LN);
-               String description = person.getString("description");
-               String country = person.getString("country");
-               String image_url = person.getString("picture");
-               String id = person.getString(ID);
+		peopleList = json;
+		String[] nameList = new String[peopleList.length()];
+		int[] avatarList = new int[peopleList.length()];
+		;
 
-               int[] image = new int[] { R.drawable.avatar};
+		try {
+			for (int i = 0; i < peopleList.length(); i++) {
+				JSONObject person = peopleList.getJSONObject(i);
 
-               ImageLoader imageLoader = ImageLoader.getInstance();
-               imageLoader.init(ImageLoaderConfiguration.createDefault(getBaseContext()));
-               //imageLoader.displayImage("http://lorempixel.com/100/100/people/", imageView);
-               
-               //DisplayImageOptions options = new DisplayImageOptions.Builder();
-               //final Bitmap bmp = imageLoader.loadImageSync("http://lorempixel.com/100/100/people/");
-               final Bitmap[] bmp = {null};
-               
-               DisplayImageOptions options = new DisplayImageOptions.Builder().build();
-    ImageSize minImageSize = new ImageSize(120, 80);
-    imageLoader.loadImage("http://lorempixel.com/100/100/people/", minImageSize, options, new SimpleImageLoadingListener() {
-    	   @Override
-    	    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-    	        // Do whatever you want with Bitmap
-    		   bmp[0] = loadedImage;
-    	    }
-    });
+				String first_name = person.getString(FIRSTNAME);
+				String last_name = person.getString(LASTNAME);
+				String description = person.getString(DESCRIPTION);
+				String country = person.getString(COUNTRY);
+				String image_url = person.getString(PICTURE);
+				String id = person.getString(ID);
 
-               // store values in a HashMap
-               HashMap<String, Object> hm = new HashMap<String, Object>();
+				nameList[i] = first_name + " " + last_name;
+				avatarList[i] = R.drawable.avatar;
+				int[] image = new int[] { R.drawable.avatar };
 
-               hm.put("full_name", first_name+" "+last_name);
-               hm.put("first_name", first_name);
-               hm.put("last_name", last_name);
-               hm.put("description", description);
-               hm.put("country", country);
-               hm.put("id", id);
-               hm.put("image", image[0]); //Integer.toString(image[0])
-               hm.put("avatar", Integer.toString(image[0]));
-               
-               hm.put("pic_url", image_url);
+				// store values in a HashMap
+				HashMap<String, Object> hm = new HashMap<String, Object>();
 
-               oslist.add(hm);
-               list=(ListView)findViewById(R.id.list);
+				hm.put("full_name", first_name + " " + last_name);
+				hm.put("first_name", first_name);
+				hm.put("last_name", last_name);
+				hm.put("description", description);
+				hm.put("country", country);
+				hm.put("id", id);
+				hm.put("image", image[0]); // Integer.toString(image[0])
+				hm.put("avatar", Integer.toString(image[0]));
 
-               ListAdapter adapter = new ExtendedSimpleAdapter(MainActivity.this, oslist,
-                       R.layout.list_v,
-                       new String[] { "full_name", "image" }, 
-                       new int[] {R.id.names, R.id.list_avatar});
+				hm.put("pic_url", image_url);
 
-               list.setAdapter(adapter);
-               list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+				oslist.add(hm);
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
 
-                   @Override
-                   public void onItemClick(AdapterView<?> parent, View view,
-                                           int position, long id) {
-                   	
-                       //Toast.makeText(MainActivity.this, "View details of id: "+oslist.get(+position).get("id"), Toast.LENGTH_SHORT).show();
-						try {
-							Bundle bdl = new Bundle();
-							bdl.putString("id", String.valueOf(id));
-							bdl.putString("first_name", (String)oslist.get(+position).get("first_name"));
-							bdl.putString("last_name", (String)oslist.get(+position).get("last_name"));
-							bdl.putString("description", (String)oslist.get(+position).get("description"));
-							bdl.putString("country", (String)oslist.get(+position).get("country"));
-							bdl.putString("pic_url", (String)oslist.get(+position).get("pic_url"));
-							Class myClass = Class.forName("com.yjlo.test.DetailPersonActivity");
-                       	Intent myIntent = new Intent(MainActivity.this, myClass);
-                       	myIntent.putExtras(bdl);
-                       	startActivity(myIntent);
-						} catch (ClassNotFoundException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-                   }
-               });
-           }
-       } catch (JSONException e) {
-           e.printStackTrace();
-       }
+		ListAdapter adapter = new personListAdapter(nameList, avatarList);
+		list.setAdapter(adapter);
+
+		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				Bundle bdl = new Bundle();
+				bdl.putString("first_name",
+						(String) oslist.get(+position).get("first_name"));
+				bdl.putString("last_name",
+						(String) oslist.get(+position).get("last_name"));
+				bdl.putString("description", (String) oslist.get(+position)
+						.get("description"));
+				bdl.putString("country",
+						(String) oslist.get(+position).get("country"));
+				bdl.putString("pic_url",
+						(String) oslist.get(+position).get("pic_url"));
+				Intent myIntent = new Intent(MainActivity.this,
+						DetailPersonActivity.class);
+				myIntent.putExtras(bdl);
+				startActivity(myIntent);
+			}
+		});
 	}
-	
+
 	@Override
-	public void onLoaderReset(Loader<JSONArray> loader) {
-		
+	public void onLoaderReset(Loader<JSONArray> arg0) {
+		// TODO Auto-generated method stub
 	}
 
-	private static class JSONParse extends AsyncTaskLoader<JSONArray> { 
+	/*
+	 * CLASS: Get JSON Array
+	 * 
+	 * @return JSONArray (the json array get from the given url)
+	 */
+	private static class JSONParse extends AsyncTaskLoader<JSONArray> {
 		public JSONParse(Context context) {
 			super(context);
 		}
-		
+
 		@Override
 		public JSONArray loadInBackground() {
-			JSONArray json = getJSONFromUrl(url);
-            return json;
+			JSONArray json = getJSONFromUrl(MainActivity.URL);
+			return json;
 		}
-	} 
- 
-	/* Get JSON from URL
+	}
+
+	/*
+	 * METHOD: Get JSON from URL
+	 * 
 	 * @param String url
+	 * 
 	 * @return JSONArray (the json array get from the given url)
 	 */
-	public static JSONArray getJSONFromUrl(String url) {
-	    InputStream is = null;
-	    JSONArray jArr = null;
-	    String json = "";
-        // HTTP request
-        try {
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost(url);
- 
-            HttpResponse httpResponse = httpClient.execute(httpPost);
-            HttpEntity httpEntity = httpResponse.getEntity();
-            is = httpEntity.getContent();           
- 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
- 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    is, "iso-8859-1"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-            is.close();
-            json = sb.toString();
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
-        }
- 
-        // parse the string to a JSON array
-        try {
-            jArr = new JSONArray(json);
-        } catch (JSONException e) {
-            Log.e("JSON Parser", "Error parsing data " + e.toString());
-        }
- 
-        // return JSON array
-        return jArr;
-    }
-}
+	private static JSONArray getJSONFromUrl(String url) {
+		InputStream is = null;
+		JSONArray jArr = null;
+		String json = "";
+		// HTTP request
+		try {
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpPost httpPost = new HttpPost(url);
 
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			HttpEntity httpEntity = httpResponse.getEntity();
+			is = httpEntity.getContent();
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					is, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			json = sb.toString();
+		} catch (Exception e) {
+			Log.e("Buffer Error", "Error converting result " + e.toString());
+		}
+
+		// parse the string to a JSON array
+		try {
+			jArr = new JSONArray(json);
+		} catch (JSONException e) {
+			Log.e("JSON Parser", "Error parsing data " + e.toString());
+		}
+		return jArr;
+	}
+
+	/*
+	 * CLASS: View Holder
+	 * 
+	 * caches the TextView and ImageView
+	 */
+	static class ViewHolder {
+		TextView personName;
+		ImageView personAvatar;
+	}
+
+	/*
+	 * CLASS: personListAdapter
+	 * 
+	 * The implementation of BaseAdapter
+	 * @param String[] nl : nameList String Array
+	 * @param int[] il : imageList int Array
+	 */
+	class personListAdapter extends BaseAdapter {
+		private String[] nameList;
+		private int[] imgeList;
+
+		personListAdapter() {
+			nameList = null;
+			imgeList = null;
+		}
+
+		public personListAdapter(String[] nl, int[] il) {
+			nameList = nl;
+			imgeList = il;
+		}
+
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return nameList.length;
+		}
+
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder viewHolder;
+
+			if (convertView == null) {
+				// inflate the layout
+				LayoutInflater inflater = getLayoutInflater();
+				convertView = inflater.inflate(R.layout.list_v, parent, false);
+
+				//set up the ViewHolder
+				viewHolder = new ViewHolder();
+				viewHolder.personName = (TextView) convertView
+						.findViewById(R.id.names);
+				viewHolder.personAvatar = (ImageView) convertView
+						.findViewById(R.id.list_avatar);
+
+				// store the holder with the view.
+				convertView.setTag(viewHolder);
+			} else {
+				// just use the viewHolder
+				viewHolder = (ViewHolder) convertView.getTag();
+			}
+			// get the views from the ViewHolder and then set the values
+			viewHolder.personName.setText(nameList[position]);
+			viewHolder.personAvatar.setImageResource(imgeList[position]);
+
+			return convertView;
+		}
+	}
+
+}
